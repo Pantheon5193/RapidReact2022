387d750e7dc8d2f6b0e0bc88058ac6a7f3783663
Levi <5193pantheon@gmail.com>
2022-01-29 12:54:21 -0500

    Added Drive Motors

--- src/main/java/frc/robot/subsystems/DriveTrain.java
+++ src/main/java/frc/robot/subsystems/DriveTrain.java
@@ -4,12 +4,27 @@
 
 package frc.robot.subsystems;
 
+import com.ctre.phoenix.motorcontrol.can.TalonFX;
+import com.ctre.phoenix.motorcontrol.ControlMode;
+
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
-public class DriveTrain extends SubsystemBase {
+  public class DriveTrain extends SubsystemBase {
   /** Creates a new ExampleSubsystem. */
   public DriveTrain() {} 
-//ha ha screw you
+    private TalonFX fl = new TalonFX(0);
+    private TalonFX fr = new TalonFX(0);
+    private TalonFX bl = new TalonFX(0);
+    private TalonFX br = new TalonFX(0);
+
+
+  public void setPower(double leftP, double rightP){
+    fl.set(ControlMode.PercentOutput, leftP);
+    fr.set(ControlMode.PercentOutput, rightP);
+    bl.set(ControlMode.PercentOutput, leftP);
+    br.set(ControlMode.PercentOutput, rightP);
+  }
+  
   @Override
   public void periodic() {
     // This method will be called once per scheduler run
